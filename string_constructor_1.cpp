4dee69355b7b1185f02efd0b1f0a780351574ad7
iamrahatkabir <iftyrahatkabir@gmail.com>
2025-06-02 18:02:53 +0600

    String construction

---
+++ string_constructor_1.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // string s = "Hello"; //Common Way;
+
+    // string s("Hello"); //This will assign value("Hello") to s;
+
+    // string s("Hello World", 4); //This will resize the value("Hello world") and print the first 4 (Given number) character;
+
+    // string s = "Hello world";
+    // string t(s, 2); //This will ignore the first 2(Given number) value;
+
+    string s(5, 'A'); //This will assing the 'A'(Given character) 5 (Given number) times;
+
+
+
+    cout << s << endl;
+    return 0;
+}
