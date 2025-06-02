06ec9b470e689222a6b49be44cee7e23b0cf9069
iamrahatkabir <iftyrahatkabir@gmail.com>
2025-06-02 23:50:50 +0600

    Find stirng in c++

---
+++ find_string_7.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s = "programming";
+
+
+    if(s.find("gram") != -1)
+    {
+        cout << "Found" << endl;
+    }
+    else{
+        cout << "Not Found" << endl;
+    }
+
+    return 0;
+}
