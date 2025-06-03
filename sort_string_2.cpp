59a458fcbc9e66a1a9205cbfc091d83749e55548
Rahat Kabir Ifty <iftyrahatkabir@gmail.com>
2025-06-04 02:13:52 +0600

    Update sort_string_2.cpp

--- sort_string_2.cpp
+++ sort_string_2.cpp
@@ -11,3 +11,13 @@ int main()
 
     return 0;
 }
+
+// int main() {
+//     string str = "phitron";
+
+//     // Sort characters from index 2 to 5 (i.e., 'i', 't', 'r', 'o')
+//     sort(str.begin() + 2, str.begin() + 6); //[Note: always add +1 to the last value while doing manually sorting, reversing and so on];
+
+//     cout << str << endl;  // Output: "phiornt"
+//     return 0;
+// }
